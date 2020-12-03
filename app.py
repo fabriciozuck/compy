1acfe16f7f4b7245eea06d19b50bb39ce7c9c882
fabriciodearaujo <fabricioaraujo4000@gmail.com>
2020-12-03 20:35:36 -0300

    Meu primeiro commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+
+drive = webdriver.Chrome(executable_path=os.getcwd()+os.sep + "chromedriver")
+drive.get("https://mercadolivre.com.br")
+
+campo_pesquisa = drive.find_element_by_xpath("//input[@class='nav-search-input']")
+
+campo_pesquisa.click()
+campo_pesquisa.send_keys("Fone Gamer Razer")
+campo_pesquisa.send_keys(Keys.ENTER)
+

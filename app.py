4fc4e1f5b0a75f5a7f97d6494a849e5a650c0414
Fabrício Araújo <44102615+fabriciodearaujo@users.noreply.github.com>
2020-12-03 22:46:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,4 @@ campo_pesquisa = drive.find_element_by_xpath("//input[@class='nav-search-input']
 campo_pesquisa.click()
 campo_pesquisa.send_keys("Fone Gamer Razer")
 campo_pesquisa.send_keys(Keys.ENTER)
+yvfuy

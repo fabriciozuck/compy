70c6f413682fd20e58113882d6a910048c959ca0
Matheus Araújo <61164981+matheusdearaujo@users.noreply.github.com>
2020-12-05 01:32:52 -0300

    Ambiente virtual/ Script de instalaçao.

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#Criando ambiente virtual
+echo "Criando ambiente virtual"
 python -m venv venv
 
-#Ativando ambiente virtual
-source /venv/bin/activate
+echo "Ativando ambiente virtual"
+source venv/bin/activate
 
-#Atualizando o pip
+echo "Atualizando o pip"
 pip install --upgrade pip
 
-#Instalando pacotes do requirements.txt
+echo "Instalando pacotes do requirements.txt"
 python -m pip install -r requirements.txt

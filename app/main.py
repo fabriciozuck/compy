b2628426010a36b25c2cab46a9df092912fefc4b
matheusdearaujo <kaienmatheus@gmail.com>
2020-12-05 21:43:52 -0300

    Pesquisando os sites de forma dinamica.

--- app/main.py
+++ app/main.py
@@ -12,18 +12,25 @@ site = int(input("-> Escolha o site que você quer pesquisar: "))
 
 if site == 1:
     link = "https://mercadolivre.com.br"
+    search = "//input[@class='nav-search-input']"
+
     prod = input("-> Digite o nome do produto que deseja pesquisa: ")
+
 elif site == 2:
     link = "https://pt.aliexpress.com/"
+    search = "//input[@class='search-key']"
+
     prod = input("-> Digite o nome do produto que deseja pesquisa: ")
+
 else:
     print("Você não selecionou nenhuma das opções.")
+    quit()
 
 
 drive = webdriver.Chrome(executable_path=os.getcwd()+os.sep + "driver/chromedriver")
 drive.get(link)
 
-campo_pesquisa = drive.find_element_by_xpath("//input[@class='nav-search-input']")
+campo_pesquisa = drive.find_element_by_xpath(search)
 
 campo_pesquisa.click()
 campo_pesquisa.send_keys(prod)

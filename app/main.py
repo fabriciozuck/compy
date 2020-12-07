28b2b396f29c61e51a025a151e6d2022227ddaf7
matheusdearaujo <kaienmatheus@gmail.com>
2020-12-06 21:40:32 -0300

    Trazendo os nomes dos produtos.

--- app/main.py
+++ app/main.py
@@ -17,33 +17,42 @@ site = int(input("-> Escolha o site que você quer pesquisar: "))
 if site == 1:
     link = "https://mercadolivre.com.br"
     search = "//input[@class='nav-search-input']"
-    list_prod = "//h2[@class='ui-search-item__title']"
+    list_prod = "//h2"
 
     prod = input("-> Digite o nome do produto que deseja pesquisa: ")
 
 elif site == 2:
     link = "https://pt.aliexpress.com/"
     search = "//input[@id='search-key']"
+    list_prod = "//a[@class='item-title']"
 
     prod = input("-> Digite o nome do produto que deseja pesquisa: ")
 
 elif site == 3:
     link = "https://www.americanas.com.br/"
     search = "//input[@class='src-input']"
+    list_prod = "//span[@class='src__Text-sc-154pg0p-0 src__Name-sc-1di8q3f-3 fLqTUE']"
 
     prod = input("-> Digite o nome do produto que deseja pesquisa: ")
 
 elif site == 4:
     link = "https://www.amazon.com.br/"
     search = "//input[@class='nav-input']"
+    list_prod = "//span[@class='a-size-base-plus a-color-base a-text-normal']"
+
+    prod = input("-> Digite o nome do produto que deseja pesquisa: ")
 
 elif site == 5:
     link = "https://www.magazineluiza.com.br/"
     search = "//input[@class='field-input-search ui-autocomplete-input']"
+    list_prod = "//h3[@class='productTitle']"
+
+    prod = input("-> Digite o nome do produto que deseja pesquisa: ")
 
 elif site == 6:
     link = "https://www.kabum.com.br/"
     search = "//input[@class='sprocura']"
+    list_prod = "//a[@class='sc-fzoLsD gnrNhT item-nome']"
 
     prod = input("-> Digite o nome do produto que deseja pesquisa: ")
 
